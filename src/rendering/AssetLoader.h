fe727213e043ca861acb67c5220f288e7534378d
Remoh210 <bogbca@gmail.com>
2020-10-14 00:06:39 -0700

    osx fixes

--- src/rendering/AssetLoader.h
+++ src/rendering/AssetLoader.h
@@ -14,21 +14,21 @@ public:
 
 	static unsigned int TextureFromFile(String path);
 
-	static void AssetLoader::LoadModel(const String& fileName,
+	static void LoadModel(const String& fileName,
 		Array<IndexedModel>& models, Array<uint32>& modelMaterialIndices,
 		Array<Material>& materials);
 
 private:
 
-	static void AssetLoader::ProcessNode(aiNode *node, const aiScene *scene, const String& fileName,
+	static void ProcessNode(aiNode *node, const aiScene *scene, const String& fileName,
 		Array<IndexedModel>& models, Array<uint32>& modelMaterialIndices,
 		Array<Material>& materials);
 
-	static void AssetLoader::ProcessMesh(aiMesh* mesh, const aiScene* scene, const String& fileName,
+	static void ProcessMesh(aiMesh* mesh, const aiScene* scene, const String& fileName,
 		Array<IndexedModel>& models, Array<uint32>& modelMaterialIndices,
 		Array<Material>& materials);
 
-	static void AssetLoader::LoadMaterialTextures(const String& filePath, aiMaterial *mat, aiTextureType type, Material& material, std::string typeName);
+	static void LoadMaterialTextures(const String& filePath, aiMaterial *mat, aiTextureType type, Material& material, std::string typeName);
 
 	static String mModelDirectory;
 	static Array<String*> mLoadedTextures;
