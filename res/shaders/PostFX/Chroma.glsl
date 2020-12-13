19116b35094ce39c30d2e70e3ad4dcf60eca2824
Remoh210 <bogbca@gmail.com>
2020-12-12 21:38:18 -0800

    chrom sheder update

--- res/shaders/PostFX/Chroma.glsl
+++ res/shaders/PostFX/Chroma.glsl
@@ -17,34 +17,48 @@ in vec2 UV;
 in vec3 WorldPos;
 
 uniform sampler2D Texture;
-uniform float Time;
+//uniform float Time;
+
+// Above fragment shader.
+uniform float     uStrength;
+uniform float     uSize;
+uniform float     uFalloff;
 
 
 out vec4 FragColor;
 
 void main()
 {		
-	float amount = 0.0;
+	// float amount = 0.0;
 	
-	amount = (1.0 + sin(Time*6.0)) * 0.5;
-	amount *= 1.0 + sin(Time*2.0) * 0.5;
-	amount *= 1.0 + sin(Time*9.0) * 0.5;
-	amount *= 1.0 + sin(Time*8.0) * 0.5;
+	// amount = (1.0 + sin(Time*6.0)) * 0.5;
+	// amount *= 1.0 + sin(Time*2.0) * 0.5;
+	// amount *= 1.0 + sin(Time*9.0) * 0.5;
+	// amount *= 1.0 + sin(Time*8.0) * 0.5;
 
-	//amount *= 1.0 + sin(Time*16.0) * 0.5;
-	//amount *= 1.0 + sin(Time*19.0) * 0.5;
-	//amount *= 1.0 + sin(Time*27.0) * 0.5;
-	amount = pow(amount, 3.0);
+	// //amount *= 1.0 + sin(Time*16.0) * 0.5;
+	// //amount *= 1.0 + sin(Time*19.0) * 0.5;
+	// //amount *= 1.0 + sin(Time*27.0) * 0.5;
+	// amount = pow(amount, 3.0);
 
-	amount *= 0.05;
+	// amount *= 0.05;
 	
-    vec3 col;
-    col.r = texture( Texture, vec2(UV.x+amount,UV.y) ).r;
-    col.g = texture( Texture, UV ).g;
-    col.b = texture( Texture, vec2(UV.x-amount,UV.y) ).b;
+    // vec3 col;
+    // col.r = texture( Texture, vec2(UV.x+amount,UV.y) ).r;
+    // col.g = texture( Texture, UV ).g;
+    // col.b = texture( Texture, vec2(UV.x-amount,UV.y) ).b;
 
-	col *= (1.0 - amount * 0.5);
-	
-    FragColor = vec4(col,1.0);
+	// col *= (1.0 - amount * 0.5);
+
+	vec2 fromCenter = UV - vec2(0.5f, 0.5f);
+	float dist = length(fromCenter);
+	float vignette = 1.0f - smoothstep(uSize, uSize - uFalloff, dist);
+	vec2 rOffset = fromCenter * vignette * uStrength;
+	float r = texture(Texture, UV + rOffset).r;
+	float g = texture(Texture, UV).g;
+	vec2 bOffset = -rOffset;
+	float b = texture(Texture, UV + bOffset).b;
+
+    FragColor = vec4(r, g, b, 1.0f);
 }
 #endif
--- src/Core/Graphics/PostFX/ChromaticAberration.h
+++ src/Core/Graphics/PostFX/ChromaticAberration.h
@@ -17,7 +17,10 @@ public:
 		drawParams.primitiveType = PRIMITIVE_TRIANGLES;
 		drawParams.shouldWriteDepth = false;
 		playbackTime += dt;
-		shader->SetUniform1f("Time", 0.5f * playbackTime);
+		//shader->SetUniform1f("Time", 0.5f * playbackTime);
+		shader->SetUniform1f("uStrength", 0.005f);
+		shader->SetUniform1f("uSize", 0.80f); // how far from the edge the effect will start
+		shader->SetUniform1f("uFalloff", 0.85f); //how far  will spread towards the centre
 		shader->SetSampler("Texture", *From->GetTexture(), *sampler, 0);
 		renderDevice->Draw(To->GetId(), shader->GetId(), VAO->GetId(), drawParams, 1, VAO->GetNumIndices());
 	}
